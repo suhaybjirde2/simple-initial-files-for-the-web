7d26b8f4f3317f5e84250cbeb85597fad20ceb60
Suhayb Jirde <dribblekay@gmail.com>
2023-01-22 05:35:59 -0800

    delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-let h1 = document.querySelector('h1');
-h1.addEventListener('click', ()=> {
-    let div = document.createElement('div');
-    div.innerHTML = "well done";
-    div.className = 'styled'
-    document.body.appendChild(div)
-})
